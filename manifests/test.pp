ade0984ec0c042919ccef642628b1941a1f252fb
Marek <marekpastierik15@gmail.com>
2019-10-22 13:45:47 +0200

    NEW FILES

---
+++ manifests/test.pp
@@ -0,0 +1,28 @@
+node "testovaci.stroj.cerit-sc.cz" {
+
+  # NASTAVENIE MENA A HESLA K API
+  class { "icinga2::api":
+      users     => ["meno-api"],
+      passowrds => ["api-heslo"],
+      urls      => ["https://monitor.ics.muni.cz:5665"], # INSTANCIA ICINGY2
+  }
+  
+  icinga2::icinga2_host { "testovaci.stroj.cerit-sc.cz":
+    ensure               => "present",
+    check_command        => "hostalive",
+    address              => "127.0.0.1",
+    templates            => ["generic-host"],
+    enable_notifications => true,
+  }
+
+  $services = lookup('icinga2::services::nrpe')
+  
+  $services.each |$name, $parameters| {
+    icinga2::icinga2_service { "check_nrpe_${name}":
+      check_command          => "nrpe",
+      enable_notifications   => true,
+      notification_users     => ["user.ics.muni.cz"],
+      vars                   => { "nrpe_port" => 5666, "nrpe_command" => $name },
+    } 
+  }
+}
